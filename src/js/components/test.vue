971e4502a1c353f1c66b0febb50c30ea65f2c5f8
Yuki Arai <sk80332@gmail.com>
2017-07-15 16:40:55 -0600

    set up vue and webpack

---
+++ src/js/components/test.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="this">
+		{{message}}
+	</div>
+</template>
+<script>
+	export default{
+		data() {
+			return {
+				message: 'ho'
+			}
+		}
+	}
+</script>
+<style>
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = {
+	entry: './src/js/main.js',
+	output:{
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			}
+		]
+	}
+};
